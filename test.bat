62b87c519ea8f4ba5833fb59c1f28fb220f2ff46
1mm0rt41PC <guillaumenuel@hotmail.com>
2020-11-11 02:37:05 +0100

    Update

--- test.bat
+++ test.bat
@@ -10,5 +10,10 @@ set keylen=64
 set DEBUG_BATCH=1
 set _7Z_OUPUT_=%scriptpath%\bin
 set _7Z_PASSWORD_=PimpMyPowny
+%py64%\python.exe -m pip install -U pip wheel ldap3 pywin32 pypiwin32
+git clone https://github.com/pyinstaller/pyinstaller --depth 1
+cd pyinstaller
+%py64%\python.exe -m pip install -r requirements.txt
+%py64%\python.exe -m pip install .
 set pyi=%py64%\Scripts\pyinstaller.exe
 %pyi%
