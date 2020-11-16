6f9a5f25d2fcad92c34ab6a2f661d0a7518c3be4
1mm0rt41PC <5358076+1mm0rt41PC@users.noreply.github.com>
2020-11-16 21:13:33 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,8 +4,8 @@ set scriptpath=%scriptpath:~0,-1%
 IF "%APPVEYOR_BUILD_FOLDER%" NEQ "^%APPVEYOR_BUILD_FOLDER^%" SET scriptpath=%APPVEYOR_BUILD_FOLDER%
 IF "%scriptpath%" == "" SET scriptpath=%CD%
 IF %scriptpath% == ^%scriptpath^% SET scriptpath=%CD%
-set py64=C:\Python39-x64
-set py32=C:\Python39
+set py64=C:\Python36-x64
+set py32=C:\Python36
 set keylen=64
 set DEBUG_BATCH=1
 set _7Z_OUPUT_=%scriptpath%\bin
