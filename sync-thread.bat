ff06a19d0b54a95281f605ec2753c7c0fdbbc511
1mm0rt41PC <guillaumenuel@hotmail.com>
2020-11-18 14:16:32 +0100

    Fix arch error

--- sync-thread.bat
+++ sync-thread.bat
@@ -3,8 +3,7 @@ IF "%MAX_THREAD%" == "" SET MAX_THREAD=%BUILDER_NB_THREAD%
 
 :: Sync threading
 IF "%BUILDER_THREADING%" == "1" (
-	CALL log.bat "Waiting thread..."
-	powershell -exec bypass -nop -Command "Get-Process -Name cmd | Where-Object { $_.MainWindowTitle -like '*%BUILDER_THREADING_TITLE%*' } | select MainWindowTitle"
-	powershell -exec bypass -nop -Command "while ( (Get-Process -Name cmd | Where-Object { $_.MainWindowTitle -like '*%BUILDER_THREADING_TITLE%*' }).Count -gt %MAX_THREAD% ){ Write-Host "Waiting for threads"; Get-Process -Name cmd | Where-Object { $_.MainWindowTitle -like '*%BUILDER_THREADING_TITLE%*' } | select MainWindowTitle; sleep -Seconds 5 }"
+	CALL log.bat "Sync thread..."
+	powershell -exec bypass -nop -Command "while ( (Get-Process -Name cmd | Where-Object { $_.MainWindowTitle -like '*%BUILDER_THREADING_TITLE%*' }).Count -gt %MAX_THREAD% ){ Write-Host -BackgroundColor Magenta "Waiting for threads"; Get-Process -Name cmd | Where-Object { $_.MainWindowTitle -like '*%BUILDER_THREADING_TITLE%*' } | select MainWindowTitle; sleep -Seconds 5 }"
 )
 EXIT /B 0
