72793898e0ef836c140a378baa637663d2afc5e4
1mm0rt41PC <guillaumenuel@hotmail.com>
2020-12-02 16:55:47 +0100

    Fix git error & add flag for threading

--- clone.bat
+++ clone.bat
@@ -5,7 +5,7 @@ CALL config.bat
 :: @param Github repo without the prefix https://github.com/
 set _git_repo=%~1
 set _git_folder=%_git_repo::=_%
-set _git_folder=%tmp%\%_git_repo:/=_%
+set _git_folder=%tmp%\%_git_folder:/=_%
 rmdir /s /q %_git_folder%
 IF %_git_repo:https://github.com=_% == %_git_repo% (
 	git clone https://github.com/%_git_repo% --depth 1 %_git_folder%
