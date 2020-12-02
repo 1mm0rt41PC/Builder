b0e3f8611b35f375766f64b6e642b9db51ca973c
1mm0rt41PC <guillaumenuel@hotmail.com>
2020-12-02 17:00:06 +0100

    add flag PYTHONOPTIMIZE_FLAG

--- build-py.bat
+++ build-py.bat
@@ -47,7 +47,7 @@ EXIT /B 0
 :Build_arch_main
 	CALL log.bat "Building %_outTarget%_%_arch%.exe ..."
 
-	%_pyExe% -OO -m PyInstaller --key=%pykey% --icon=%scriptpath%\pytools.ico --onefile %_pyTarget%.py --name %_outTarget%_%_arch% --noupx
+	%_pyExe% %PYTHONOPTIMIZE_FLAG% -m PyInstaller --key=%pykey% --icon=%scriptpath%\pytools.ico --onefile %_pyTarget%.py --name %_outTarget%_%_arch% --noupx
 	IF NOT EXIST "dist\%_outTarget%_%_arch%.exe" CALL log.bat ERR "Build %_outTarget%_%_arch%.exe FAIL" 1
 	dist\%_outTarget%_%_arch%.exe -h
 	set _err=%ERRORLEVEL%
