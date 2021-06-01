347405287320d146fb0a0b753b88900ac244106d
1mm0rt41PC <5358076+1mm0rt41PC@users.noreply.github.com>
2021-06-01 14:29:26 +0200

    Update main.bat

--- main.bat
+++ main.bat
@@ -104,13 +104,18 @@ CALL log.bat "Building Rubeus..."
 msbuild /property:Configuration=Release
 CALL log.bat "Create Rubeus.7z with required files..."
 cd Rubeus\bin\release
-IF EXIST Rubeus (
+Rubeus.exe -h
+set _err=%ERRORLEVEL%
+set _errorExpected=0
+IF "%ERRORLEVEL%" == "%_errorExpected%" (
 	CALL log.bat "✅ Build Rubeus.exe OK" 1
 	echo Rubeus.exe >Rubeus.lst7z
 	echo Rubeus.exe.config >>Rubeus.lst7z
 	7z a -t7z -mhe -p%_7Z_PASSWORD_% %_7Z_OUPUT_%\Rubeus.7z @Rubeus.lst7z
-	appveyor PushArtifact Rubeus.7z
+	appveyor PushArtifact %_7Z_OUPUT_%\Rubeus.7z
 	copy Rubeus.exe %scriptpath%\bin\Rubeus.exe
+) else (
+	CALL log.bat ERR "FAIL to build a valid Rubeus.exe (This bin return %_err%, expected %_errorExpected%)..." , 1
 )
 
 :: Sync threading
