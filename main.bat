e20e5bdd38eaff09c8914ee97fb6aa39c358152d
1mm0rt41PC <5358076+1mm0rt41PC@users.noreply.github.com>
2022-07-13 00:23:35 +0200

    Update main.bat

--- main.bat
+++ main.bat
@@ -44,8 +44,8 @@ CALL clone.bat skelsec/pypykatz
 :: https://skelsec.medium.com/play-with-katz-get-scratched-6c2c350fadf2
 :: https://drive.google.com/drive/folders/1KT2yWziJHvaH41jtZMsatey2KycWF824?usp=sharing
 :: From https://github.com/skelsec/pypykatz/commit/f53ed8c691b32c2a5a0189604d56afe4732fb639
-git am %scriptpath%\pypykatz\BruteForcer.patch
-git am %scriptpath%\pypykatz\Add-debug-message-for-method-handledup.patch
+::git am %scriptpath%\pypykatz\BruteForcer.patch
+::git am %scriptpath%\pypykatz\Add-debug-message-for-method-handledup.patch
 cd pypykatz
 CALL build-py.bat __main__ , pypykatz , 0
 
