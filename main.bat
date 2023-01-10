18a5337035b4563adaf61fd0dd44292c14f41f60
1mm0rt41PC <5358076+1mm0rt41PC@users.noreply.github.com>
2023-01-10 13:19:10 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -37,6 +37,9 @@ CALL clone.bat ly4k/Certipy
 cd build\lib\certipy
 CALL build-py.bat entry , certipy , 0
 
+CALL clone.bat ShutdownRepo/pywhisker
+CALL build-py.bat pywhisker , pywhisker , 0
+
 :: See https://dirkjanm.io/krbrelayx-unconstrained-delegation-abuse-toolkit/
 CALL clone.bat dirkjanm/krbrelayx
 CALL build-py.bat krbrelayx , krbrelayx , 0
