896c8e131e719b645637561e711d05c3d0e98959
1mm0rt41PC <5358076+1mm0rt41PC@users.noreply.github.com>
2023-04-28 16:25:01 +0200

    fix #6

--- main.bat
+++ main.bat
@@ -27,6 +27,7 @@ CALL build-py.bat ticketer , ticketer , 0
 CALL build-py.bat tstool , tstool , 0
 CALL build-py.bat wmiexec , wmiexec , 0
 CALL build-py.bat atexec , atexec , 0
+CALL build-py.bat ntlmrelayx , ntlmrelayx , 0
 
 CALL clone.bat Hackndo/WebclientServiceScanner
 cd webclientservicescanner
