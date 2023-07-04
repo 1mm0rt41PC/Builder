1f9afb169fb1d92bb8d25684426eadda913bfdf2
1mm0rt41PC <5358076+1mm0rt41PC@users.noreply.github.com>
2023-07-05 00:01:50 +0200

    Create build.bat

---
+++ Bloodhound/build.bat
@@ -0,0 +1,32 @@
+git clone https://github.com/1mm0rt41PC/BloodHound --depth 1
+cd BloodHound
+
+:: Adjust Collectors
+rmdir /q /s Collectors\DebugBuilds
+mv Collectors ..\
+:: Copy Collectors
+xcopy %scriptpath%\bin\certipy_x64.exe ..\Collectors\
+xcopy %scriptpath%\bin\bloodhound_x64.exe ..\Collectors\
+xcopy %scriptpath%\bin\Certify.exe ..\Collectors\
+xcopy %scriptpath%\bin\rusthound.exe ..\Collectors\
+
+npm install -g electron-packager
+npm install
+SET NODE_OPTIONS=--openssl-legacy-provider
+setx NODE_OPTIONS --openssl-legacy-provider
+setx /M NODE_OPTIONS --openssl-legacy-provider
+::npm run build:win32 --arch=x64
+npm run compile
+npm run package -- --platform=linux,win32 --arch=x64 --icon=src/img/icon.ico
+
+:: Download custom queries
+curl -L -k https://github.com/1mm0rt41PC/BloodHoundQueries/raw/master/customqueries.json --output customqueries.json
+
+:: Download Neo4J with preconfiguration
+curl.exe -k -L https://neo4j.com/artifact.php?name=neo4j-community-5.9.0-windows.zip --output neo4j.zip
+7z x neo4j.zip
+powershell -exec bypass -nop -Command "$neo4j = cat .\neo4j-community-*\conf\neo4j.conf ; if( $neo4j.Contains('#dbms.security.auth_enabled=false') ){ $neo4j.replace('#dbms.security.auth_enabled=false','dbms.security.auth_enabled=false') | Out-File -Encoding ASCII .\neo4j-community-*\conf\neo4j.conf ; }; mv neo4j-community-* neo4j-community"
+
+type %scriptpath%\Bloodhound\run.bat > run.bat
+
+7z a BloodHound-win32-x64 BloodHound-linux-x64 customqueries.json ..\Collectors neo4j-community run.bat
